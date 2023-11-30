46bc20042cc0b66f4ea43c534f8373ebc8fc22ec
Ian Effendi <effendiian@gmail.com>
2023-11-30 05:09:34 -0500

    :rotating_light: Fix trailing whitespace and line ending warnings.

--- python/E01_dockerize-python/src/tests/test_app.py
+++ python/E01_dockerize-python/src/tests/test_app.py
@@ -6,13 +6,13 @@ class TestApp(unittest.TestCase):
     """
     Unit tests for the exercise 1.
     """
-    
+
     def test_inside_container_exists(self):
         """
         Test if the `inside_container` variable exists in the application.
         """
         self.assertTrue('inside_container' in dir(app))
-        
+
     def test_ping_expects_200_response(self):
         """
         Test if the `ping()` Response returns a 200 status code.
